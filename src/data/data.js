6f8cf735e9ba75cdde39b5960f7900a0bfb8604e
Ibigelow92 <iobbigelow@gmail.com>
2023-11-20 18:59:06 -0800

    data

--- src/data/data.js
+++ src/data/data.js
@@ -1,182 +1,180 @@
 export const data = [
-    {
-      id: 1,
-      name: 'Lamb Vindaloo',
-      category: 'burger',
-      image:
-        'https://images.pexels.com/photos/2474661/pexels-photo-2474661.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-      price: '$$$$',
-    },
-    {
-      id: 2,
-      name: 'Butter Chicken',
-      category: 'burger',
-      image:
-        'https://images.pexels.com/photos/2679501/pexels-photo-2679501.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-      price: '$',
-    },
-    {
-      id: 3,
-      name: 'Mushroom Burger',
-      category: 'burger',
-      image:
-        'https://images.unsplash.com/photo-1608767221051-2b9d18f35a2f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJ1cmdlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60',
-      price: '$$',
-    },
-    {
-      id: 4,
-      name: 'Loaded Burger',
-      category: 'burger',
-      image:
-        'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60',
-      price: '$$$',
-    },
-    {
-      id: 5,
-      name: 'Feta & Spinnach',
-      category: 'pizza',
-      image:
-        'https://images.unsplash.com/photo-1593560708920-61dd98c46a4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-      price: '$$',
-    },
-    {
-      id: 6,
-      name: 'Supreme Pizza',
-      category: 'pizza',
-      image:
-        'https://images.unsplash.com/photo-1604382355076-af4b0eb60143?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-      price: '$$$',
-    },
-    {
-      id: 7,
-      name: 'Meat Lovers',
-      category: 'pizza',
-      image:
-        'https://images.unsplash.com/photo-1628840042765-356cda07504e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBpenphfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
-      price: '$$$$',
-    },
-    {
-      id: 8,
-      name: 'Cheese Pizza',
-      category: 'pizza',
-      image:
-        'https://images.unsplash.com/photo-1548369937-47519962c11a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2hlZXNlJTIwcGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-      price: '$',
-    },
-    {
-      id: 9,
-      name: 'Kale Salad',
-      category: 'salad',
-      image:
-        'https://images.unsplash.com/photo-1515543237350-b3eea1ec8082?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2FsYWQlMjBjZWFzYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-      price: '$$',
-    },
-    {
-      id: 10,
-      name: 'Ceasar Salad',
-      category: 'salad',
-      image:
-        'https://images.unsplash.com/photo-1546793665-c74683f339c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c2FsYWQlMjBjZWFzYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-      price: '$$$',
-    },
-    {
-      id: 11,
-      name: 'Loaded Salad',
-      category: 'salad',
-      image:
-        'https://images.unsplash.com/photo-1540420773420-3366772f4999?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2FsYWR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-      price: '$$$$',
-    },
-    {
-      id: 12,
-      name: 'Fruit Salad',
-      category: 'salad',
-      image:
-        'https://images.unsplash.com/photo-1564093497595-593b96d80180?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZnJ1aXQlMjBzYWxhZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
-      price: '$',
-    },
-    {
-      id: 13,
-      name: 'Wings',
-      category: 'chicken',
-      image:
-        'https://images.unsplash.com/photo-1567620832903-9fc6debc209f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-      price: '$$',
-    },
-    {
-      id: 14,
-      name: 'Baked Chicken',
-      category: 'chicken',
-      image:
-        'https://images.unsplash.com/photo-1594221708779-94832f4320d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-      price: '$$$$',
-    },
-    {
-      id: 15,
-      name: 'Chicken Tenders',
-      category: 'chicken',
-      image:
-        'https://images.unsplash.com/photo-1562967914-608f82629710?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNoaWNrZW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
-      price: '$',
-    },
-    {
-      id: 16,
-      name: 'Chicken Kabob',
-      category: 'chicken',
-      image:
-        'https://images.unsplash.com/photo-1603360946369-dc9bb6258143?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNoaWNrZW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
-      price: '$$$',
-    },
-  ];
-  
-  export const categories = [
-    {
-      id: 1,
-      name: 'Fast Food',
-      image:
-        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/FastFood_BrowseHome@3x.png',
-    },
-    {
-      id: 2,
-      name: 'Pizza',
-      image:
-        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Pizza_BrowseHome@3x.png',
-    },
-    {
-      id: 3,
-      name: 'Wings',
-      image:
-        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Wings_BrowseHome@3x.png',
-    },
-    {
-      id: 4,
-      name: 'Indian',
-      image:
-        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Indian_BrowseHome@3x.png',
-    },
-    {
-      id: 5,
-      name: 'Latest Deals',
-      image:
-        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Deals_BrowseHome@3x.png',
-    },
-    {
-      id: 6,
-      name: 'Restaurant Rewards',
-      image:
-        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/RestoRewards_BrowseHome@3x.png',
-    },
-    {
-      id: 7,
-      name: 'Best Overall',
-      image:
-        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/TopEats_Browse%20Home@3x.png',
-    },
-    {
-      id: 8,
-      name: 'Shipped Free',
-      image:
-        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Placeholder_Plates@3x.png',
-    },
-  ];
-  
+  {
+    id: 1,
+    name: "Lamb Vindaloo",
+    category: "indian",
+    image:
+      "https://images.pexels.com/photos/2474661/pexels-photo-2474661.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    price: "$$$$",
+  },
+  {
+    id: 2,
+    name: "Butter Chicken",
+    category: "indian",
+    image:
+      "https://images.pexels.com/photos/2679501/pexels-photo-2679501.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    price: "$",
+  },
+  {
+    id: 3,
+    name: "Khichdi",
+    category: "indian",
+    image:
+      "https://images.pexels.com/photos/6363501/pexels-photo-6363501.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    price: "$$",
+  },
+  {
+    id: 4,
+    name: "Mattar Paneer",
+    category: "indian",
+    image:
+      "https://images.pexels.com/photos/9609842/pexels-photo-9609842.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    price: "$$$",
+  },
+  {
+    id: 5,
+    name: "12-Piece Sushi Set",
+    category: "japanese",
+    image:
+      "https://images.pexels.com/photos/6249501/pexels-photo-6249501.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    price: "$$",
+  },
+  {
+    id: 6,
+    name: "Sashimi Set",
+    category: "japanese",
+    image:
+      "https://images.pexels.com/photos/13869853/pexels-photo-13869853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    price: "$$$",
+  },
+  {
+    id: 7,
+    name: "Japanese Curry",
+    category: "japanese",
+    image:
+      "https://images.pexels.com/photos/674574/pexels-photo-674574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    price: "$$$$",
+  },
+  {
+    id: 8,
+    name: "Spicy Udon",
+    category: "japanese",
+    image:
+      "https://images.pexels.com/photos/17952228/pexels-photo-17952228/free-photo-of-beef-ramen-with-udon-noodles-eaten-with-chopsticks.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    price: "$",
+  },
+  {
+    id: 9,
+    name: "Carnitas Taco Set",
+    category: "mexican",
+    image:
+      "https://images.pexels.com/photos/2087748/pexels-photo-2087748.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    price: "$$",
+  },
+  {
+    id: 10,
+    name: "Chicken Burrito",
+    category: "mexican",
+    image: "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg",
+    price: "$$$",
+  },
+  {
+    id: 11,
+    name: "Torta Combination",
+    category: "mexican",
+    image:
+      "https://images.pexels.com/photos/14473870/pexels-photo-14473870.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    price: "$$$$",
+  },
+  {
+    id: 12,
+    name: "Plain Quesadilla",
+    category: "mexican",
+    image:
+      "https://images.pexels.com/photos/11256292/pexels-photo-11256292.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    price: "$",
+  },
+  {
+    id: 13,
+    name: "Buffalo Wings",
+    category: "fast-food",
+    image:
+      "https://images.unsplash.com/photo-1567620832903-9fc6debc209f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+    price: "$$",
+  },
+  {
+    id: 14,
+    name: "Fried Chicken Burger",
+    category: "fast-food",
+    image:
+      "https://images.pexels.com/photos/2657960/pexels-photo-2657960.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    price: "$$$$",
+  },
+  {
+    id: 15,
+    name: "Double Cheeseburger",
+    category: "fast-food",
+    image:
+      "https://images.pexels.com/photos/13998873/pexels-photo-13998873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    price: "$",
+  },
+  {
+    id: 16,
+    name: "Pulled Pork Burger",
+    category: "fast-food",
+    image:
+      "https://images.pexels.com/photos/13998986/pexels-photo-13998986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    price: "$$$",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    name: "Fast Food",
+    image:
+      "https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/FastFood_BrowseHome@3x.png",
+  },
+  {
+    id: 2,
+    name: "Pizza",
+    image:
+      "https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Pizza_BrowseHome@3x.png",
+  },
+  {
+    id: 3,
+    name: "Wings",
+    image:
+      "https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Wings_BrowseHome@3x.png",
+  },
+  {
+    id: 4,
+    name: "Indian",
+    image:
+      "https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Indian_BrowseHome@3x.png",
+  },
+  {
+    id: 5,
+    name: "Latest Deals",
+    image:
+      "https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Deals_BrowseHome@3x.png",
+  },
+  {
+    id: 6,
+    name: "Restaurant Rewards",
+    image:
+      "https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/RestoRewards_BrowseHome@3x.png",
+  },
+  {
+    id: 7,
+    name: "Best Overall",
+    image:
+      "https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/TopEats_Browse%20Home@3x.png",
+  },
+  {
+    id: 8,
+    name: "Shipped Free",
+    image:
+      "https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Placeholder_Plates@3x.png",
+  },
+];
